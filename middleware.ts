3a10ee50c4ab706af6422a4d24f589162c7a4e3d
Krish Gupta <krshg@outlook.com>
2023-02-18 11:00:46 +0000

    chore(deps): upgrade

--- middleware.ts
+++ middleware.ts
@@ -16,44 +16,16 @@ export const config = {
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers.get("host") || "redirekt.biz";
 
-  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
-  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-        .replace(`.redirekt.biz`, "")
-      : hostname.replace(`.localhost:3000`, "");
+  const isBaseApp = process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+    ? hostname === "redirekt.biz"
+    : hostname === "localhost:3000";
 
-  // rewrites for app pages
-  if (currentHost == "app") {
-    if (
-      url.pathname === "/login" &&
-      (req.cookies.get("next-auth.session-token") ||
-        req.cookies.get("__Secure-next-auth.session-token"))
-    ) {
-      url.pathname = "/";
-      return NextResponse.redirect(url);
-    }
+  if (isBaseApp)
+    return NextResponse.next();
 
-    url.pathname = `/app${url.pathname}`;
-    return NextResponse.rewrite(url);
-  }
-
-  // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "redirect.biz") {
-    return NextResponse.rewrite(new URL(`/home${path}`, req.url));
-  }
-
-  // rewrite everything else to `/_sites/[site] dynamic route
-  return NextResponse.rewrite(
-    new URL(`/_sites/${currentHost}${path}`, req.url)
-  );
+  return NextResponse.next();
 }
--- pages/api/domain/index.ts
+++ pages/api/domain/index.ts
@@ -1,25 +1,44 @@
-import { createDomain, deleteDomain } from "@/lib/api";
-import { unstable_getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth/next";
 
 import { authOptions } from "../auth/[...nextauth]";
 import { HttpMethod } from "@/types";
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import { checkVercelDomain, createDatabaseDomain, createVercelDomain, deleteDatabaseDomain, getDatabaseDomain, getVercelDomain, verifyVercelDomain } from "@/lib/api";
 
 export default async function domain(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const session = await unstable_getServerSession(req, res, authOptions);
+  const session = await getServerSession(req, res, authOptions);
+
   if (!session) return res.status(401).end();
+
+  const domain = req.query.domain as string;
 
   switch (req.method) {
     case HttpMethod.POST:
-      return createDomain(req, res);
+      return res.status(200).json({ db: await createDatabaseDomain(domain, session), vercel: await createVercelDomain(domain) });
+
     case HttpMethod.DELETE:
-      return deleteDomain(req, res);
+      return res.status(200).json({ db: await deleteDatabaseDomain(domain), vercel: await getVercelDomain(domain) });
+
+    case HttpMethod.GET:
+      const check = !!req.query.check;
+      const verify = !!req.query.verify;
+
+      if (check) {
+        return res.status(200).json(await checkVercelDomain(domain));
+      }
+
+      if (verify) {
+        return res.status(200).json(await verifyVercelDomain(domain));
+      }
+
+      return res.status(200).json({ db: await getDatabaseDomain(domain), vercel: await getVercelDomain(domain) });
+
     default:
-      res.setHeader("Allow", [HttpMethod.POST, HttpMethod.DELETE]);
+      res.setHeader("Allow", [HttpMethod.POST, HttpMethod.DELETE, HttpMethod.GET]);
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
